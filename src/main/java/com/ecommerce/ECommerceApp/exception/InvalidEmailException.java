389487192c9f226c83f44d8b9955f9b02376d74f
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-11-03 17:55:53 -0500

    Added javadoc for exception class

--- src/main/java/com/ecommerce/ECommerceApp/exception/InvalidEmailException.java
+++ src/main/java/com/ecommerce/ECommerceApp/exception/InvalidEmailException.java
@@ -1,5 +1,8 @@
 package com.ecommerce.ECommerceApp.exception;
 
+/**
+ * Throws an exception error when invalid email address is provided.
+ */
 public class InvalidEmailException extends RuntimeException {
     public InvalidEmailException(String message) {
         super(message);
