2307669d2750c5c1f0e49a1ee8294e3bec694a48
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-11-03 17:55:14 -0500

    Added javadoc for exception class

--- src/main/java/com/ecommerce/ECommerceApp/exception/InvalidPhoneNumberException.java
+++ src/main/java/com/ecommerce/ECommerceApp/exception/InvalidPhoneNumberException.java
@@ -1,5 +1,8 @@
 package com.ecommerce.ECommerceApp.exception;
 
+/**
+ * Throws exception when invalide phone number is provided.
+ */
 public class InvalidPhoneNumberException extends RuntimeException {
     public InvalidPhoneNumberException(String message) {
         super(message);
