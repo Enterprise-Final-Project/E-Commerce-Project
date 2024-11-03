c79fe49145a76f209ea431db20ec07a706528777
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-11-03 17:28:53 -0500

    Added missing JavaDoc

--- src/main/java/com/ecommerce/ECommerceApp/model/AccountType.java
+++ src/main/java/com/ecommerce/ECommerceApp/model/AccountType.java
@@ -1,5 +1,9 @@
 package com.ecommerce.ECommerceApp.model;
 
+/**
+ * Enumerator containing different types of user account types
+ * Includes: USER, SUPPLIER_USER, SUPPLIER_ADMIN, ADMIN
+ */
 public enum AccountType {
     USER,
     SUPPLIER_USER,
